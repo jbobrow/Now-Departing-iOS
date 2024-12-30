4bdf5b890fa2e2ce122409f9345b44a49299dd16
Jonathan Bobrow <jbobrow@gmail.com>
2024-12-29 20:17:45 -0500

    Initial Commit

---
+++ Now Departing Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Now Departing Watch App
+//
+//  Created by Jonathan Bobrow on 12/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Now Departing Watch App/Now_DepartingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Now_DepartingApp.swift
+//  Now Departing Watch App
+//
+//  Created by Jonathan Bobrow on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Now_Departing_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
