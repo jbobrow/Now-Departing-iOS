bbe203924de6c794f1fc454f53030e0de94a1515
Jonathan Bobrow <jbobrow@gmail.com>
2025-06-08 11:00:51 -0400

    Initial Commit

---
+++ Now Departing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Now Departing
+//
+//  Created by Jonathan Bobrow on 6/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Now Departing/Now_DepartingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Now_DepartingApp.swift
+//  Now Departing
+//
+//  Created by Jonathan Bobrow on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Now_DepartingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
